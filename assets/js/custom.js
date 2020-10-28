8f7ab3beea8b0b0c89a5660091308c313cef6436
Your Name <you@example.com>
2020-10-28 22:23:15 +0900

    Blogdown201028

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,11 +1,1 @@
 
-tocbot.init({
-  // Where to render the table of contents.
-  tocSelector: '.js-toc',
-  // Where to grab the headings to build the table of contents.
-  contentSelector: '.article-style',
-  // Which headings to grab inside of the contentSelector element.
-  headingSelector: 'h1, h2, h3',
-  // For headings inside relative or absolute positioned containers within content.
-  hasInnerContainers: true,
-});
